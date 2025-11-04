fbab89c6df46e713103163114eb8dcd580390a56
mixxfm1076 <63142109+mixxfm1076@users.noreply.github.com>
2025-11-04 20:11:26 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Configuration Vite pour Mixx Radio SPA
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  base: './',
 })
